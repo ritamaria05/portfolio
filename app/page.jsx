0a33e330d6fed1bf48a25b08287c85becb74c484
Rita Moreira <rita.moreira.1414@gmail.com>
2025-12-23 22:38:32 +0000

    From TSX to JSX and update page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <section id="me"> {/* About Me + Skills */}
+
+      </section>
+      <section id="projects"> {/* Projects */}
+
+      </section>
+      <section id="contacts"> {/* Contacts */}
+
+      </section>
+    </main>
+  );
+}
